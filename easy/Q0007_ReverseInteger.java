05ec77599cedd36938c7e69c673b6264388e3da1
Shreya Gupta <iamshreyagupta13@gmail.com>
2026-07-30 21:10:25 +0530

    Add Solution or LeetCode #268

--- easy/Q0007_ReverseInteger.java
+++ easy/Q0007_ReverseInteger.java
@@ -11,10 +11,10 @@ public class Q0007_ReverseInteger {
     int rev= 0;
     while(num !=0){
         rem = num % 10;
-        if(rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 &&  rev > 7)){
+        if(rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 &&  rem > 7)){
             return 0;
         }
-        if(rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && rev < -8)){
+        if(rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && rem < -8)){
             return 0;
         }
         rev = rev * 10 + rem;
---
+++ easy/Q268_MissingNumbers.java
@@ -0,0 +1,18 @@
+package easy;
+public class Q268_MissingNumbers{
+    public static void main(String args[]){
+        int nums[] = {0,2,3};
+       System.out.println(missingNumber(nums));
+
+    }
+    public static  int missingNumber(int nums[]){
+        int n = nums.length;
+        int actualSum = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            actualSum = actualSum + nums[i];
+        }
+
+        int expectedSum = (n * (n + 1)) / 2;
+        return expectedSum - actualSum;
+    }
+}
