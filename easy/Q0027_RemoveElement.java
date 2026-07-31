79dd7942ae770f5bf21c5d096a8af5ddcb4aabe0
Shreya Gupta <iamshreyagupta13@gmail.com>
2026-07-31 23:18:39 +0530

    Add Solution For LeetCode #27

---
+++ easy/Q0027_RemoveElement.java
@@ -0,0 +1,24 @@
+package easy;
+
+public class Q0027_REmoveElement {
+    public static void main(String[] args) {
+        int nums[] = {1,2,4,5,2};
+        int result = removeElement(nums, 2);
+        System.out.println("k = " + result);
+        for(int i = 0;i<result;i++){
+            System.out.print(nums[i] + " ");
+        }
+       
+         
+    }
+    public static int removeElement(int nums[] , int val){
+        int k = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
