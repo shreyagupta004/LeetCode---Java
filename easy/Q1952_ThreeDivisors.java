0e46be2d6765119e0d3fe430ed29b8eb4e012f84
Shreya Gupta <iamshreyagupta13@gmail.com>
2026-08-01 22:10:17 +0530

    Add Solution For Leetcode #1952

---
+++ easy/Q1952_ThreeDivisors.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class Q1952_ThreeDivisors {
+    public static void main(String[] args) {
+        System.out.println(threeDivisors(4));
+        
+    }
+    public static boolean threeDivisors(int num){
+        int count = 0;
+        for(int i = 1 ; i <= num ; i++){
+           if(num % i == 0 ){
+            count++;
+           }
+        }
+        if(count == 3){
+             return true;
+         }else{
+            return false;
+           }
+        
+    
+    }
+}
