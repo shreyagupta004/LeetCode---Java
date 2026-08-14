265d0c2173aa64af61e89d50d329cd306af23213
Shreya Gupta <iamshreyagupta13@gmail.com>
2026-08-14 17:31:02 +0530

    solution for #1920

---
+++ easy/Q1920_BuildArrayFromPermutation.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class Q1920_BuildArrayFromPermutation {
+    public static void main(String[] args) {
+        int nums[] = {0,2,1,5,3,4};
+        int result []= buildArray(nums);
+        for(int i = 0; i < nums.length ; i++){
+            System.out.print(result[i] + ",");
+        }
+        
+    }
+    public static int[] buildArray(int nums[]){
+        int arr[] = new int[nums.length];
+        for(int i = 0; i < nums.length ; i++){
+            arr[i] = nums[nums[i]];
+        }
+        return arr;
+    }
+}
