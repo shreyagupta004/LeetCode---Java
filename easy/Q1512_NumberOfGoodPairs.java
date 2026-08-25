eaff403e3f1670920d0a1ec6ccd9e19f24aae1f7
Shreya Gupta <iamshreyagupta13@gmail.com>
2026-08-25 21:42:38 +0530

    solution for #1512

---
+++ easy/Q1512_NumberOfGoodPairs.java
@@ -0,0 +1,26 @@
+package easy;
+
+public class Q1512_NumberOfGoodPairs {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,1,1,3};
+        int result = numIdenticalPairs(nums);
+        System.out.println(result);
+        
+    }
+    public static int numIdenticalPairs(int[] nums){
+
+        int count = 0;
+
+        for(int i = 0 ; i < nums.length  ;i++){
+
+            for(int j = 0 ; j < nums.length ; j++){
+
+                if(nums[i] == nums[j] && i < j){
+
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
