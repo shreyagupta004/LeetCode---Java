d8be711e1c20b46d54f78786ba5000164cb58dd3
Shreya Gupta <iamshreyagupta13@gmail.com>
2026-09-16 23:47:27 +0530

    solution for #657

---
+++ easy/Q0657_RobotReturnsToOrigin.java
@@ -0,0 +1,35 @@
+package easy;
+
+public class Q0657_RobotReturnsToOrigin {
+    public static void main(String[] args) {
+
+        String s = "UDLDRLR";
+        boolean res = judgeCircle(s);
+        System.out.println(res);
+        
+    }
+    public static boolean judgeCircle(String s){
+        int x = 0;
+        int y = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            
+            if(s.charAt(i) == 'U'){
+                y++;
+            }
+            else if(s.charAt(i) == 'D'){
+                y--;
+            }
+            else if(s.charAt(i) == 'L'){
+                x--;
+            }
+            else if(s.charAt(i) == 'R'){
+                x++;
+            }
+            // if(x == 0 && y == 0){
+            //     return true;
+            // }
+            
+        }
+        return x == 0 && y == 0;
+    }
+}
